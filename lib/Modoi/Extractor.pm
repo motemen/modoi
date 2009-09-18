dc64061a9217a33f776e65193052ff9db0c729f5
motemen <motemen@gmail.com>
2009-09-18 20:59:19 +0900

    Extractor

--- lib/Modoi/Extractor.pm
+++ lib/Modoi/Extractor.pm
@@ -1,21 +1,53 @@
 package Modoi::Extractor;
 use Any::Moose;
 
-use Web::Scraper;
+use HTML::TreeBuilder::XPath;
+use YAML;
+use Path::Class qw(file);
+use List::MoreUtils qw(uniq);
 
 __PACKAGE__->meta->make_immutable;
 
 no Any::Moose;
 
-# TODO site specific extractor
 sub extract {
     my ($self, $res) = @_;
 
-    my $scraper = scraper {
-        process '//img', 'images[]' => '@src';
-    };
+    my $content = $res->content;
+    my $config = $self->site_config;
+    my @result;
 
-    $scraper->scrape($res);
+    my $tree;
+
+    foreach my $rule (@{$config->{rules}}) {
+        if (my $xpath = $rule->{xpath}) {
+            $tree ||= do {
+                my $tree = HTML::TreeBuilder::XPath->new;
+                $tree->parse($content);
+                $tree;
+            };
+            push @result, map { $_->string_value } $tree->findnodes($xpath);
+        } elsif (my $regexp = $rule->{regexp}) {
+            while ($content =~ /($regexp)/g) {
+                my $fragment = $1;
+                my $uri = URI->new_abs($fragment, $res->base);
+                if ($rule->{rewrite}) {
+                    my @matches = ($fragment, $fragment =~ /$regexp/);
+                    $uri = $rule->{rewrite};
+                    $uri =~ s/\$(\d+)/$matches[$1]/ge;
+                }
+                push @result, $uri;
+            }
+        }
+    }
+
+    uniq @result;
+}
+
+# TODO
+sub site_config {
+    my ($self, $uri) = @_;
+    YAML::LoadFile file(qw(assets 2chan.net extractor.yaml));
 }
 
 1;
---
+++ t/extractor.t
@@ -0,0 +1,27 @@
+use strict;
+use warnings;
+use Test::More 'no_plan';
+
+use HTTP::Response;
+use HTTP::Status;
+use HTTP::Request::Common;
+use Path::Class qw(file);
+
+BEGIN { use_ok 'Modoi::Extractor' }
+
+my $extractor = Modoi::Extractor->new;
+
+my $res = HTTP::Response->new(
+    RC_OK, status_message(RC_OK),
+    [ Content_Type => 'text/html' ],
+    scalar file('t/samples/img.2chan.net-b-res-69762910.htm')->slurp,
+);
+$res->request(GET 'http://img.2chan.net/b/res/69762910.htm');
+
+my @extracted = $extractor->extract($res);
+is_deeply \@extracted, [qw[
+    http://www.nijibox5.com/futabafiles/tubu/src/su232756.jpg
+    http://www.nijibox5.com/futabafiles/tubu/src/su232759.jpg
+    http://feb.2chan.net/img/b/thumb/1253268691967s.jpg
+    http://feb.2chan.net/img/b/src/1253268691967.jpg
+]];
