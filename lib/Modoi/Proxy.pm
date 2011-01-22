753401f539d5588f47ef3d2711804dfc821f9bf7
motemen <motemen@gmail.com>
2011-01-23 01:38:12 +0900

    remove proxy headers

--- lib/Modoi/Proxy.pm
+++ lib/Modoi/Proxy.pm
@@ -10,6 +10,9 @@ has fetcher => (
 
 sub serve {
     my ($self, $req) = @_;
+    foreach ($req->headers->header_field_names) {
+        $req->headers->remove_header($_) if /^Proxy-/i;
+    }
     return $self->fetcher->request($req);
 }
 
