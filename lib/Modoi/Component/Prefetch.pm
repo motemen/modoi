cd558707ecc80465abe973ea0654f6c8b789e058
motemen <motemen@gmail.com>
2011-01-26 13:51:24 +0900

    Component::Prefetch sleep をはさむ

--- lib/Modoi/Component/Prefetch.pm
+++ lib/Modoi/Component/Prefetch.pm
@@ -14,6 +14,7 @@ package Modoi::Fetcher::Role::Prefetch;
 use Mouse::Role;
 use Modoi;
 use Coro;
+use Coro::Timer qw(sleep);
 
 after modify_response => sub {
     my ($self, $res, $req) = @_;
@@ -22,11 +23,17 @@ after modify_response => sub {
 
     my @media = Modoi->component('FindMedia')->find_media($res, $req->request_uri);
     async {
+        my $i = 0;
         foreach my $url (@media) {
             next if Modoi->component('Cache')->has_cache($url);
-            Modoi->log(notice => "prefetch $url ...");
+            Modoi->log(notice => "prefetch $url");
             Modoi->fetcher->fetch($url);
+            if (++$i % 5 == 0) {
+                Modoi->log(notice => "prefetched $i, sleep for 3 secs... (from " . $req->request_uri . ')');
+                sleep 3;
+            }
         }
+        Modoi->log(info => "total $i prefetch done (from " . $req->request_uri . ')');
     } if @media;
 };
 
